ac127781bc9ce96158c915bfa1397a02485fc95d
Željko Bilandžija <cigara9393@hotmail.com>
2018-09-12 20:47:31 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { createLogger } from 'redux-logger';
+// import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import 'tachyons';
